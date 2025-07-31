64b6a8d4885b1b71897c6330269e6fd56891cc85
Parthiv Badreshiya <parthivbadreshiya@gmail.com>
2025-07-31 12:43:48 +0530

    Cleaned up duplicate backend folder after merge

--- middlewares/utils/sendNotification.js
+++
@@ -1,7 +0,0 @@
-export const sendEmail = async (admin, type) => {
-    console.log(`[EMAIL] To: ${admin.email} | Type: ${type}`);
-};
-
-export const sendSMS = async (phone, type) => {
-    console.log(`[SMS] To: ${phone} | Type: ${type}`);
-};
--- models/settings/payments/PaymentMethod.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const paymentMethodSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }, // UPI, Card, PayPal, etc.
-    type: { type: String, enum: ['UPI', 'Card', 'PayPal', 'Bank', 'Other','Online','COD'], required: true },
-    isActive: { type: Boolean, default: true },
-}, { timestamps: true });
-
-export default mongoose.model('PaymentMethod', paymentMethodSchema);
