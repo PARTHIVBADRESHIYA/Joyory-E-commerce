bb2788f44f110f643088beacb579845ab0216f09
PARTHIVBADRESHIYA <164191495+PARTHIVBADRESHIYA@users.noreply.github.com>
2025-08-23 17:01:05 +0530

    Remove duplicate folders outside backend

--- joyory-e-commerce/backend/middlewares/cache.js
+++
@@ -1,5 +0,0 @@
-// middlewares/cache.js
-import NodeCache from 'node-cache';
-
-// Create cache with TTL: 10 mins (600 seconds)
-export const cache = new NodeCache({ stdTTL: 600, checkperiod: 120 });
