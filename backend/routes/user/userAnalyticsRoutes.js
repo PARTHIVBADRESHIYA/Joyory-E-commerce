92f5dd1474d56ab7ed134d49bcf03042a6329dfb
Parthiv Badreshiya <parthivbadreshiya@gmail.com>
2026-01-20 17:29:01 +0530

    route change

--- backend/routes/user/userAnalyticsRoutes.js
+++ backend/routes/user/userAnalyticsRoutes.js
@@ -15,7 +15,7 @@ router.get("/all", isAdmin, getAllCustomerAnalytics);
 router.get("/:userId", isAdmin, getCustomerAnalytics);
 
 router.get("/activities/all", isAdmin, getAllUserActivities);
-router.get("/activities/:type", isAdmin, getActivitiesByType);  
 router.get("/activities/:userId", isAdmin, getUserActivitiesByUser);
+router.get("/activities/:type", isAdmin, getActivitiesByType);  
 
 export default router;
